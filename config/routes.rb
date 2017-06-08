66d89c864ac192378dae80591443db8662e68141
Josh Kuehn <joshmkuehn@gmail.com>
2017-06-08 07:36:17 -0500

    generated Sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :projects
   root 'projects#index'
   get '/:type', to: 'projects#type'
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
 
 end
