f581d4deb5776649952d6b24bab49f1c407c81e3
Josh Kuehn <joshmkuehn@gmail.com>
2017-06-11 23:09:57 -0500

    Fix to type

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   validates :title, :description, :projType, presence: true
   validates :projType, length: { maximum: 1 }
-  attr_access :projImage_file_name
+  attr_accessor :projImage_file_name
 
   has_attached_file :projImage, styles: {
     thumb: '100x100>',
