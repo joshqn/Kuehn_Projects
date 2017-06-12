b969a40145bea32ea56a2188d4aefd9ef492a6fa
Josh Kuehn <joshmkuehn@gmail.com>
2017-06-11 23:08:06 -0500

    Added attr_access to projImage_file_name

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,7 @@
 class Project < ApplicationRecord
   validates :title, :description, :projType, presence: true
   validates :projType, length: { maximum: 1 }
+  attr_access :projImage_file_name
 
   has_attached_file :projImage, styles: {
     thumb: '100x100>',
